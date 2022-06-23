818c34c4ad78532bfa55f40f81e5fcbb002c65d7
basararasai <xfernandez_de_labastida@uoc.edu>
2022-06-23 16:37:39 +0200

    main page changes

--- scripts.js
+++ scripts.js
@@ -1,6 +1,11 @@
 const toggleMenuElement = document.getElementById('toggle-menu');
 const mainMenuElement = document.getElementById('main-menu');
+const titleElement = document.getElementById('title');
 
 toggleMenuElement.addEventListener('click', () => {
     mainMenuElement.classList.toggle('main-menu--show');
+});
+
+toggleMenuElement.addEventListener('click', () => {
+    titleElement.classList.toggle('title-show');
 });
