a2e8c650f88f4e58ba6c590c208c8f1a596b14a3
basararasai <xfernandez_de_labastida@uoc.edu>
2022-06-25 10:54:45 +0200

    create all main pages and fix design problems

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
 const toggleMenuElement = document.getElementById('toggle-menu');
 const mainMenuElement = document.getElementById('main-menu');
 const titleElement = document.getElementById('title');
+const subtitleElement= document.getElementById('subtitle');
+const logoElement = document.getElementById('logo');
 
 toggleMenuElement.addEventListener('click', () => {
     mainMenuElement.classList.toggle('main-menu--show');
@@ -8,4 +10,12 @@ toggleMenuElement.addEventListener('click', () => {
 
 toggleMenuElement.addEventListener('click', () => {
     titleElement.classList.toggle('title-show');
+});
+
+toggleMenuElement.addEventListener('click', () => {
+    subtitleElement.classList.toggle('subtitle');
+});
+
+toggleMenuElement.addEventListener('click', () => {
+    logoElement.classList.toggle('logo-show');
 });
